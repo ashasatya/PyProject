c84c00a7cbae06f1b52a49a8cc853809a93025d8
Asha Satyakumar <asha.ks@gmail.com>
2017-12-05 14:36:06 -0800

    Adding the changes to enter value from terminal

--- PrimeFactor.py
+++ PrimeFactor.py
@@ -24,8 +24,15 @@ def prime_factor(num):
     return list_of_factors
 
 #num = sys.argv[1:0]
+value = sys.argv[1:]
+if value != []:
+    number = value[0]
+    PrimeFactors = prime_factor(int(number))
+    print(f'list of prime factors for {number} are: ', PrimeFactors)
+else:
+    print('Rerun the Primefactor program from the terminal and enter the number for which primefactor has to be found')
 
 #num = int(input("Enter the number to find the prime factors: "))
-PrimeFactors = prime_factor(600851475143)
-print(f'list of prime factors for 600851475143 are: ', PrimeFactors)
+#PrimeFactors = prime_factor(600851475143)
+#print(f'list of prime factors for 600851475143 are: ', PrimeFactors)
 
